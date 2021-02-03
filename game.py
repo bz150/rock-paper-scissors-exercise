039a8cbc8d769f0aefaeca2c8d2eb2152444081c
Bryan Zhou <bz150@georgetown.edu>
2021-02-03 18:19:48 -0500

    Validating user input
    End of class on 2/3. Changing user input to lowercase not working properly yet

--- game.py
+++ game.py
@@ -16,18 +16,52 @@ print("You chose:", user_choice) #printing multiple things separated by comma
 #print(f"You chose: {user_choice}") #string interpolation
 
 #simulating computer input
-
+print("-------------------")
 options = ['rock', 'paper', 'scissors']
 computer_choice = random.choice(options)
-
 print("The computer chose:", computer_choice)
 
-exit()
+#verify valid selection from user
+#stop the program and don't determine winner if user choice is invalid
+user_choice.lower()
 
+print(user_choice)
+quit()
+
+#if user_choice in options:
+#    #pass through
+#else:
+#    print("Please choose a valid option and try again")
+#    exit()
+
+quit()
 #determining who won
-
 print("-------------------")
-print("Oh, the computer won. It's ok.")
+
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("Oh, it's a tie.")
+    elif computer_choice == "paper":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "scissors":
+        print("Oh, you won! Nice job.")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "paper":
+        print("Oh, it's a tie.")
+    elif computer_choice == "scissors":
+        print("Oh, the computer won. It's ok.")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("Oh, the computer won. It's ok.")
+    elif computer_choice == "paper":
+        print("Oh, you won! Nice job.")
+    elif computer_choice == "scissors":
+        print("Oh, it's a tie.")
+else:
+    print("OOPS SOMETHING WENT WRONG.")
+
 
 print("-------------------")
 
